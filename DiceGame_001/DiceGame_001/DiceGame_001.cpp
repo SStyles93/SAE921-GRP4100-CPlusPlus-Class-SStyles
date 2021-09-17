d5ca574b60cff2b0808bd4b8c9a45013f2e37156
SStyles93 <samuel.styles93@gmail.com>
2021-09-17 18:35:28 +0200

    Added a bet logic

--- DiceGame_001/DiceGame_001/DiceGame_001.cpp
+++ DiceGame_001/DiceGame_001/DiceGame_001.cpp
@@ -6,6 +6,7 @@
 int main()
 {
 	int balance = 5;
+	int bet;
 	int dice1, dice2;
 	int minScore = 7;
 	int currentScore = 7;
@@ -13,7 +14,8 @@ int main()
 	std::string playerName = "Human";
 	std::string currency = " CHF";
 	std::string yesOrNo = "default";
-	
+
+	bool hasBet = false;
 	bool isPlaying = false;
 
 	//Greet the player and ask for his name
@@ -49,33 +51,60 @@ int main()
 	//Start of game loop
 	do 
 	{
+		std::cout << "How much do you want to bet ?" << std::endl;
+		do
+		{
+			std::cin >> bet;
+			if (bet > balance) 
+			{
+				std::cout << "You do not have that much money " << playerName << std::endl;
+				bet = 0;
+				std::cout << "How much do you want to bet ?" << std::endl;
+			}
+			else if ( bet <= balance)
+			{
+				balance -= bet;
+			}
+			if (!std::isdigit(bet))
+			{
+				std::cout << "That is not a number..." << std::endl;
+				bet = 0;
+				std::cout << "How much do you want to bet ?" << std::endl;
+				
+			}
+		} while (bet <= 0);
+
 		srand(time(0));
 
-		dice1 = (rand() % 6 + 1);
-		dice2 = (rand() % 6 + 1);
+		dice1 = 2;//(rand() % 6 + 1);
+		dice2 = 5;//(rand() % 6 + 1);
 
 		std::cout << "Your first dice score is : " << dice1 << " Your second dice score is : " << dice2 << std::endl;
 		int totalScore = dice1 + dice2;
 		std::cout << "So...your total dice score is :" << totalScore << std::endl;
 
-		if (totalScore > currentScore)
+		if (totalScore >= currentScore)
 		{
 			currentScore = totalScore;
-			balance *= 2;
-			std::cout << "Congratulations! You have doubled your fortune" << std::endl;
+			balance += bet*2;
+			bet = 0;
+			std::cout << "Congratulations! You have doubled your bet" << std::endl;
 			std::cout << "You now have : " << balance << currency << std::endl;
+			std::cout << "Do you want to play again ? [y/n]" << std::endl;
+			std::cin >> yesOrNo;
+			if (yesOrNo == "n" || yesOrNo == "N")
+			{
+				std::cout << "Ok, see you another time" << std::endl;
+				isPlaying = false;
+			}
+
 		}
-		if(totalScore <= minScore)
+		if(totalScore <= minScore && balance <= 0)
 		{
-			std::cout << "Sorry my friend, you are broke..." << std::endl;
-			return EXIT_SUCCESS;
+			std::cout << "Sorry " << playerName << ", you are broke..." << std::endl;
+			isPlaying = false;
 		}
 
-		//TODO: Continue game logic for Wining cases
-
-		/*std::cout << "isPlaying" << std::endl;
-		is playing has to be conditionnaly set*/
-		isPlaying = false;
 	} while (isPlaying == true);
 
 	
