070126d7b22c8b6d51acfdc228713000a62ddefd
SStyles93 <samuel.styles93@gmail.com>
2021-09-19 05:06:18 +0200

    Update DiceGame_DragonVariant.cpp

--- DiceGame_DragonVariant/DiceGame_DragonVariant/DiceGame_DragonVariant.cpp
+++ DiceGame_DragonVariant/DiceGame_DragonVariant/DiceGame_DragonVariant.cpp
@@ -9,7 +9,7 @@ class Player
 {
 public:
 	std::string name = "Theobald";
-	std::string description = "The Noble Knight";
+	std::string description = " \"The Noble Knight\" ";
 
 	int health = 100;
 	int attackValue = 5;
@@ -19,14 +19,16 @@ public:
 	bool isDefending = false;
 		
 };
-class Npc
+class Enemy
 {
 public:
 	std::string name = "Grimgor";
-	std::string description = "The Black Orc";
+	std::string description = " \"The Black Orc\" ";
+
 	int health = 100;
 	int attackValue = 2;
 };
+
 //Methods
 void Clear()
 {
@@ -43,8 +45,8 @@ void Clear()
 int main()
 {
 	//Game Variables
-	Player theo;
-	Npc grim;
+	Player player;
+	Enemy enemy;
 	std::string separationLine = "____________________________________________\n";
 	std::string yesOrNo = "default";
 	bool isGame = false;
@@ -54,10 +56,10 @@ int main()
 	//Greet player and ask if he wants to play
 	std::cout << "Welcome to the DragonVariant of the DiceGame" << std::endl;
 	std::cout << separationLine << std::endl;
-	std::cout << "Your are " << theo.name << " \"" << theo.description << "\"" << std::endl;
-	std::cout << "You currently have " << theo.health << " health points" << std::endl;
-	std::cout << "Your opponent is " << grim.name << " \"" << grim.description << "\"" << std::endl;
-	std::cout << grim.name << " has " << grim.health << " health points" << std::endl;
+	std::cout << "Your are " << player.name << player.description << std::endl;
+	//std::cout << "You currently have " << player.health << " health points" << std::endl;
+	std::cout << "Your opponent is " << enemy.name << enemy.description << std::endl;
+	//std::cout << enemy.name << " has " << enemy.health << " health points" << std::endl;
 	std::cout << separationLine << std::endl;
 
 	//Get player input. Exit or Play and Re-ask if input is not correct
@@ -73,9 +75,8 @@ int main()
 		}
 		//Play
 		else if (yesOrNo == "y" || yesOrNo == "Y")
-		{	
-			////UNCOMMENT CLEAR() WHEN GAME FINISHED
-			//Clear();
+		{
+			Clear();
 			isGame = true;
 		}
 		//Keep asking until we get a correct answer
@@ -86,27 +87,26 @@ int main()
 	} while (yesOrNo != "y" && yesOrNo != "Y");
 
 	//Start Game Loop
-	do 
+	do
 	{
 		//Exit conditions
-		if (theo.health <= 0)
+		if (player.health <= 0)
 		{
-			std::cout << theo.name << " Won!" << std::endl;
+			std::cout << enemy.name << enemy.description << " Won!" << std::endl;
 			return EXIT_SUCCESS;
 		}
-		if (grim.health <= 0)
+		if (enemy.health <= 0)
 		{
-			std::cout << grim.name << "Won!" << std::endl;
+			std::cout << player.name << enemy.description << "Won!" << std::endl;
 			return EXIT_SUCCESS;
 		}
 		//Player turn
 		do
 		{
-			//UNCOMMENT CLEAR() AFTER TEST
-			Clear();
+			//Clear();
 			std::cout << separationLine << std::endl;
-			std::cout << "You currently have " << theo.health << " health points" << std::endl;
-			std::cout << grim.name << " has " << grim.health << " health points" << std::endl;
+			std::cout << "You have " << player.health << " health points" << std::endl;
+			std::cout << enemy.name << " has " << enemy.health << " health points" << std::endl;
 			std::cout << separationLine << std::endl;
 			std::cout << "It is your turn to play!" << std::endl;
 			std::cout << "enter[1] to Attack" << std::endl;
@@ -114,24 +114,33 @@ int main()
 			std::cout << "enter [3] to Heal yourself" << std::endl;
 			std::cout << separationLine << std::endl;
 			std::cout << "Select your action" << std::endl;
-			std::cin >> theo.action;
-			
-			switch (theo.action)
+			std::cin >> player.action;
+			Clear();
+
+			//Action selection
+			switch (player.action)
 			{
 			case 1:
-				std::cout << theo.name << " attacks " << grim.name << std::endl;
-				grim.health -= theo.attackValue;
+				std::cout << player.name << " attacks " << enemy.name << std::endl;
+				enemy.health -= player.attackValue;
+				std::cout << enemy.name << " looses " << player.attackValue << " health" << std::endl;
 				isPlayerTurn = false;
 				isNpcTurn = true;
 				break;
 			case 2:
-				std::cout << theo.name << " uses his shield" << std::endl;
-				theo.isDefending = true;
+				std::cout << player.name << " uses his shield" << std::endl;
+				player.isDefending = true;
 				isPlayerTurn = false;
 				isNpcTurn = true;
 				break;
 			case 3:
-				theo.health += theo.healValue;
+				std::cout << player.name << " heals up" << std::endl;
+				player.health += player.healValue;
+				if (player.health >= 100) 
+				{
+					player.health = 100;
+				}
+				std::cout << "and recovers " << player.healValue << " health" << std::endl;
 				isPlayerTurn = false;
 				isNpcTurn = true;
 				break;
@@ -141,42 +150,66 @@ int main()
 				isPlayerTurn = true;
 				continue;
 			}
+
 		} while (isPlayerTurn);
 
 		//NPC turn
 		do
 		{
-			//UNCOMMENT CLEAR() AFTER TEST
-			Clear();
+			//Output health and attack
+			//Clear();
 			std::cout << separationLine << std::endl;
-			std::cout << "You currently have " << theo.health << " health points" << std::endl;
-			std::cout << grim.name << " has " << grim.health << " health points" << std::endl;
+			std::cout << "You have " << player.health << " health points" << std::endl;
+			std::cout << enemy.name << " has " << enemy.health << " health points" << std::endl;
 			std::cout << separationLine << std::endl;
-			std::cout << grim.name << " attacks " << theo.name << std::endl;
+			std::cout << enemy.name << " attacks " << player.name << std::endl;
 			//Random seed
 			srand(time(0));
-			//Get a random attack value between 2 and 8
-			grim.attackValue = ((rand() % 8) + 2);
+			//Get a random attack value (%7 = [0-6] // + 2 = [2-8]
+			enemy.attackValue = ((rand() % 7) + 2);
+			//
+			std::cout << enemy.attackValue << std::endl;
 			//Return damage with a 10% probability of doubling
 			int probability = (rand() % 100) < 10;
 			if (probability == 1)
 			{
-				std::cout << grim.name << " summoned the Waagh" << std::endl;
-				grim.attackValue *= 2;
-				theo.health -= grim.attackValue;
+				std::cout << enemy.name << " summoned the Waagh" << std::endl;
+				enemy.attackValue *= 2;
+				if (player.isDefending)
+				{
+					enemy.attackValue *= 70;
+					enemy.attackValue /= 100;
+					player.health -= enemy.attackValue;
+					player.isDefending = false;
+				}
+				else
+				{
+
+					player.health -= enemy.attackValue;
+				}
 			}
 			else
 			{
-				theo.health -= grim.attackValue;
+				if (player.isDefending)
+				{
+					enemy.attackValue *= 70;
+					enemy.attackValue /= 100;
+					player.health -= enemy.attackValue; 
+					player.isDefending = false;
+				}
+				else
+				{
+					player.health -= enemy.attackValue;
+				}
 			}
-			std::cout << grim.attackValue << std::endl;
+			std::cout << player.name << " looses " << enemy.attackValue << " health" << std::endl;
 			isNpcTurn = false;
 			isPlayerTurn = true;
 
 		} while (isNpcTurn);
 
 	} while (isGame);
-	
+
 
 	return 0;
 }
