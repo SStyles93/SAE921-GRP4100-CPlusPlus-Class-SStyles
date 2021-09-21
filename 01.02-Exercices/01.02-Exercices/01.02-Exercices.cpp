7da711c68a747610be9c0980ccca77be4df2a8d2
SStyles93 <samuel.styles93@gmail.com>
2021-09-21 09:11:33 +0200

    Update 01.02-Exercices.cpp

--- 01.02-Exercices/01.02-Exercices/01.02-Exercices.cpp
+++ 01.02-Exercices/01.02-Exercices/01.02-Exercices.cpp
@@ -6,6 +6,7 @@ enum class animals
     CAT,
     DOG,
     COW,
+    HORSE
 };
 
 std::string getAnimalsNoise(animals animal) 
@@ -21,8 +22,11 @@ std::string getAnimalsNoise(animals animal)
     case animals::COW:
         return "Moo";
         break;
+    case animals::HORSE:
+        return "?";
+        break;
     default:
-        return "-";
+        return "None";
         break;
     }
 }
@@ -41,6 +45,9 @@ std::string getAnimalsName(animals animal)
     case animals::COW:
         return "Cow";
         break;
+    case animals::HORSE:
+        return "Horse";
+        break;
     default:
         return "-";
         break;
@@ -51,39 +58,52 @@ std::string getAnimalsName(animals animal)
 
 int main()
 {
+    // Access enum and print animals list
     std::cout << "Welcome to the animal noise dictionnary" << std::endl;
     std::cout << "There are currently : " << sizeof(animals)-1 << " in the dictionnary" << std::endl;
     std::cout << "Those are : " << std::endl;
 
-    int len = sizeof(animals);
-    for (int i = 0; i < len-1; i++)
+    int len = sizeof(animals)-1;
+    for (int i = 0; i < len; i++)
     {
         std::cout << "[" << i << "]" << getAnimalsName(animals(i)) <<std::endl;
     }
 
-    std::cout << "Select the animal you want to hear the noise from" << std::endl;
-    int wantedAnimalIdx;
+    std::string yesOrNo;
     do
     {
-        std::cin >> wantedAnimalIdx;
-        if (wantedAnimalIdx > sizeof((animals)-1))
+        std::cout << "Select the animal you want to \"hear\" the noise from" << std::endl;
+        int wantedAnimalIdx;
+        do
         {
-            std::cin.clear();
-            std::cin.ignore();
-            std::cout << "Enter a valid index" << std::endl;
-            continue;
-        }
-        else if (wantedAnimalIdx < sizeof(animals))
-        {
-            std::cin.clear();
-            std::cin.ignore();
-            continue;
-        }
-        else 
-        {
-            std::cout << "The " << getAnimalsName(animals(wantedAnimalIdx)) << "\'s noise is: "
-                << getAnimalsNoise(animals(wantedAnimalIdx)) << std::endl;
-        }
-    } while (wantedAnimalIdx > sizeof((animals)-1) || wantedAnimalIdx < 0);
+            std::cin >> wantedAnimalIdx;
+            if (wantedAnimalIdx > sizeof((animals)-1))
+            {
+                std::cin.clear();
+                std::cin.ignore();
+                std::cout << "Enter a valid index" << std::endl;
+                continue;
+            }
+            else if (!std::cin)
+            {
+                //Ignore anything that isn't a number
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "That is not a number...\n" << "Enter a valid index" << std::endl;
+                wantedAnimalIdx = (sizeof(animals) + 1);
+                continue;
+            }
+            else
+            {
+                std::cout << "The " << getAnimalsName(animals(wantedAnimalIdx)) << "\'s noise is: "
+                    << getAnimalsNoise(animals(wantedAnimalIdx)) << std::endl;
+            }
+        } while (wantedAnimalIdx > sizeof((animals)-1));
+        //Exit?
+        std::cout << "Do you want to exit? [y/n]" << std::endl;
+        std::cin >> yesOrNo;
+    } while (yesOrNo != "y" && yesOrNo != "Y");
 
-    }
+    std::cout << "Ok, see you another time" << std::endl;
+    return EXIT_SUCCESS;
+}
