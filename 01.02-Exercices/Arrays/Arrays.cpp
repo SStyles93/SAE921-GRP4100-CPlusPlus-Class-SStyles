50f077799baca2d47e22a252fd14128b5445302a
SStyles93 <samuel.styles93@gmail.com>
2021-09-22 14:30:49 +0200

    Rename and end Arrays

--- 01.02-Exercices/Arrays/Arrays.cpp
+++ 01.02-Exercices/Arrays/Arrays.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+//Array_Ex1 Variables & Functions
 int scores[]{ 84, 92, 76, 81, 56 };
-
 int BestScores() 
 {
 	int bestScore = 0;
@@ -16,9 +18,77 @@ int BestScores()
 	}
 	return bestScore;
 }
-int main()
-{
-    
 
+int main()
+{	
+#pragma region ArrayEx1
+/*
+	//ArrayEx1
     std::cout << BestScores();
+	*/
+#pragma endregion
+#pragma region Array2
+/*
+	//ArrayExercice 2
+
+	unsigned int V;
+	do
+	{
+		std::cout << "Input Value between 0 and 20" << std::endl;
+		std::cin >> V;
+		
+	} while (V > 20);
+	
+	const int nb = 10;
+	int randomArray[nb];
+
+	bool isIn = false;
+
+	srand(time(0));
+	std::cout << "{";
+	for (size_t i = 0; i < nb; i++)
+	{
+		randomArray[i] = ((rand() % 20) + 1);
+		std::cout << randomArray[i] << ", ";
+		if (randomArray[i] == V) 
+		{
+			isIn = true;
+		}
+	}
+	std::cout << "}";
+	if (isIn == true) 
+	{
+		std::cout << std::endl;
+		std::cout << V << " is in the array";
+	}
+	if (isIn == false) 
+	{
+		std::cout << std::endl;
+		std::cout << V << " is not it the array" << std::endl;
+	}
+	*/
+#pragma endregion
+#pragma region ArrayEx3
+	/*
+	//ArrayEx_3
+	const int numRows = 13;
+	const int numCols = 13;
+	//Defined Array
+	int mulTable[numRows][numCols]{};
+	
+		for (int row = 1; row < numRows; ++row)
+		{
+			std::cout << row << " : ";
+
+			for (int col = 1; col < numCols; ++col)
+			{
+				std::cout << col * row << "|";
+			}
+			std::cout << std::endl;
+		}
+		*/
+#pragma endregion
+
+
 }
+
--- README.md
+++ README.md
@@ -9,11 +9,15 @@ The studied language is C++.
 DiceGame_001 is a simple dice game where the player has to enter his name, chose to play or exit and then wish for the best!
 
 DONE:	
-	-Finish DiceGame_001: (DONE) - Clear() console ?
-	-Do a DragonVariant: (DONE)
+	-Finish DiceGame_001: 
+		-Clear() console ?
+		-Do a DragonVariant:
 	-Written analysis:
-		-DiceGame_001 (DONE)
-		-DiceGame_DragonVariant (DONE)
+		-DiceGame_001
+		-DiceGame_DragonVariant
+	Exercice 01.02:
+		-Enum
+		-Arrays 1,2
 
 TODO:
 	-Do a DiceGame Variant (2Players, Jackpot, BlackJack).
