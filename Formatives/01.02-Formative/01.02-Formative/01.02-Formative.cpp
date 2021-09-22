97a5b6b913bb87a9e9d23eccb9751507a1a0be1e
SStyles93 <samuel.styles93@gmail.com>
2021-09-22 23:35:02 +0200

    Update 01.02-Formative.cpp

--- Formatives/01.02-Formative/01.02-Formative/01.02-Formative.cpp
+++ Formatives/01.02-Formative/01.02-Formative/01.02-Formative.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-/*
+
 enum class Value 
 {
 	AS, TWO, TREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING,
@@ -14,30 +14,49 @@ enum class Suit
 
 const int suitLenght = static_cast<int>(Suit::SUITCOUNT);
 const int valueLenght = static_cast<int>(Value::DECKSIZE);
-int deckSize[suitLenght][valueLenght];
-int deck[suitLenght*valueLenght];
-int CreateDeck() 
+int deck2D[suitLenght][valueLenght]{};
+int deck[suitLenght * valueLenght]{}; 
+
+void CreateDeck() 
 {
 	int deckCount = 0;
 
-	for (int suit = 0; suit < suitLenght; suit++)
+	for (int suit{ 0 }; suit < suitLenght; ++suit)
 	{
-		for (int val = 0; val < valueLenght ; val++)
+		for (int val{ 0 }; val < valueLenght; ++val)
 		{
 			deckCount++;
-			deck[deckCount] = suit, val;
+			deck2D[suit][val] = deckCount;
+			deck[deckCount] = deck2D[suit][val];
 		}
 	}
-	return 0;
+}
+void PrintDeck() 
+{
+	int _deckCount = 0;
+	for (size_t i = 0; i < suitLenght; ++i)
+	{
+		for (size_t j = 0; j < valueLenght; ++j)
+		{
+			_deckCount++;
+			//std::cout << deck2D[i][j];
+			std::cout << deck[_deckCount];
+		}
+		std::cout << std::endl;
+	}
 }
 int GetCard(int cardNumber_)
 {
+	//Get the card with %suitLenght or %valueLenght..or smth like that..
+	//and loops ?
 	return deck[cardNumber_];
 }
 
 int main()
 {
 	CreateDeck();
-	std::cout << GetCard(1);
+	//Print Deck
+	PrintDeck();
+	//Create a function to print out the card
+	std::cout << "out the card" << std::endl;
 }
-*/
