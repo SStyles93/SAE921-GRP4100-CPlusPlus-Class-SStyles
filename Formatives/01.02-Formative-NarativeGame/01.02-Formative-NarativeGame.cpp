df4d979aa389047c3b1f0ca904bfc2011fec5755
SStyles93 <samuel.styles93@gmail.com>
2021-09-23 12:33:22 +0200

    Formative-NarativeGame first commit

---
+++ Formatives/01.02-Formative-NarativeGame/01.02-Formative-NarativeGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+/*
+Sarah Shepard, Born on planet Earth at 24 / 08 / 2087. Captain of the crusader ship The Explorer
+Phil spector, Born on planet Mars at 26 / 08 / 2038. Pilot of the H - Hunter n° 3434 - DFG
+Whifghy, the metamorph.Created  in Sector SD / F67.Child of the bioship Sxiot
+*/
+
+std::string name[3] = { "Sarah Shepard", "Phil Spector", "Whifghy", };
+std::string planet[3] = { "Earth", "Mars", "Sector SD / F67" };
+std::string title[3] = { "Captain", "Pilot", "Child" };
+int main()
+{
+    std::cout << "Hello World!\n";
+}
