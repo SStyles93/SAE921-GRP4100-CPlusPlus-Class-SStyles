abc3a84fb70ab694fb5b087913946f3a3ae5d283
SStyles93 <samuel.styles93@gmail.com>
2021-09-29 14:13:42 +0200

    Update 02-Exercice.cpp

--- 02-Exercice/02-Exercice/02-Exercice.cpp
+++ 02-Exercice/02-Exercice/02-Exercice.cpp
@@ -1,47 +1,25 @@
 #include <iostream>
-bool test;
-bool a = false;
-bool b = false;
-bool c = false;
 
-void Test(bool test_) 
-{
-    a = false, b = false;
-    std::cout << a << " | " << b;
-    std::cout << " =  " << test_ << std::endl;
-    a = false, b = true;
-    std::cout << a << " | " << b;
-    std::cout << " =  " << test_ << std::endl;
-    a = true, b = false;
-    std::cout << a << " | " << b;
-    std::cout << " =  " << test_ << std::endl;
-    a = true, b = true;
-    std::cout << a << " | " << b;
-    std::cout << " =  " << test_ << std::endl;
-}
 int main()
 {
+    bool test;
 
 #pragma region Exercice-1.1
     //Exercice-1.1
     //(a&&b)||(!a&&b)
-    Test((a && b) || (!a && b));   
-    std::cout << std::endl;
 
 #pragma endregion
 
 #pragma region Exercice-1.2-Formative
     //Exercice-1.2-Formative
     //!(a&&!b)||(!a||b)
-    Test(!(a && !b) || (!a || b));
-    std::cout << std::endl;
+    
 #pragma endregion
 
 #pragma region Exercice-1.3-Formative
     //Exercice-1.3-Formative
     //(a&&c)||(!a||b)&&!(c||!b)
-    Test((a && c) || (!a || b) && !(c || !b));
-    std::cout << std::endl;
+    
 #pragma endregion
 
 #pragma region Exercice-2.1
@@ -51,6 +29,7 @@ int main()
     // 1 | 0 = 0
     // 1 | 1 = 1
 
+    //Res: b
     
 
 #pragma endregion
@@ -62,8 +41,17 @@ int main()
     // 1 | 0 = 1 
     // 1 | 1 = 1 
 
-    //std::cout << test << std::endl;
+    //
 
 #pragma endregion
+
+#pragma region Exercice-2.2-Formative
+    //Exercice-2.2-Formative
+    // 0 | 0 = 0
+    // 0 | 1 = 0
+    // 1 | 0 = 1
+    // 1 | 1 = 1
+#pragma endregion
+
 
 }
