1d6b80d791b505f0d2e9dd06d838b2b696ce65d4
SStyles93 <samuel.styles93@gmail.com>
2021-10-01 20:01:20 +0200

    Update 03.04-Formative-Map.cpp

--- 03.04-Formative-Map/03.04-Formative-Map.cpp
+++ 03.04-Formative-Map/03.04-Formative-Map.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <iostream>
+#include <string>
 #include <map>
 
 char exitChar;
@@ -30,7 +30,7 @@ void AskForKeyValuePair()
 		concatNames.clear();
 		phoneNumber.clear();
 
-		std::cout << "Do you want to continue adding people to the list ? [n] to exit";
+		std::cout << "Do you want to continue adding people to the list ? [n] to exit\n";
 		std::cin >> exitChar;
 	}
 }
@@ -41,7 +41,33 @@ void PrintMap()
 		std::cout << mapIt->first << " : " << mapIt->second << std::endl;
 	}
 }
+void FindNumberInMap(std::string name_)
+{
+	std::map<std::string, std::string>::iterator it;
+	for (it = map.begin(); it != map.end(); it++)
+	{
+		if (it->first == name_) 
+		{
+			std::cout << it->second << std::endl;
+		}
+		else
+		{
+			std::cout << "Nothing was found" << std::endl;
+		}
+	}
+}
+
 int main()
 {
+	
 	AskForKeyValuePair();
+	PrintMap();
+
+	std::string name = "Default";
+	std::cout << "Who's number are you looking for ? \n";
+	std::cin.ignore();
+	std::getline(std::cin, name);
+	
+
+	FindNumberInMap(name);
 }
