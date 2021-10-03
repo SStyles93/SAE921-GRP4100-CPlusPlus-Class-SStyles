8286f2fa7e67d33ab54862fb9ea4f95fded421e8
SStyles93 <samuel.styles93@gmail.com>
2021-10-03 11:18:12 +0200

    Update 03.02 - Formative - RouletteRusse.cpp

--- 03.02 - Formative - RouletteRusse/03.02 - Formative - RouletteRusse.cpp
+++ 03.02 - Formative - RouletteRusse/03.02 - Formative - RouletteRusse.cpp
@@ -14,57 +14,68 @@ void FillChamber()
 	//Test
 	/*for (size_t i = 0; i < chamber.size(); i++)
 	{
-		chamber[i] = i;
-	}
-	*/
+		chamber[i] = 1;
+	}*/
+	
 }
-void SpinCylinder() 
+void ScrollChamber() 
 {
-	std::cout << "Spin !" << std::endl;
-	std::vector<int>::iterator it = chamber.begin();
 	int temp;
-	//number of time the data scrollings happens
-	for (size_t i = 0; i < rand(); i++)
+
+	//scrollings data
+	for (auto it = chamber.begin(); it != chamber.end(); it++)
 	{
-		//scrollings data
-		for (auto it = chamber.begin(); it != chamber.end(); it++)
+		//get the first iterators value and store it in a "temp" variable
+		if (it == chamber.begin())
 		{
-			//get the first iterators value and store it in a "temp" variable
-			if (it == chamber.begin())
-			{
-				temp = *it;
-			}
-			//if the iterators arrives to the last element 
-			//of the vector, puts the temp variable in it
-			if (it == chamber.end()-1)
-			{
-				*(chamber.end()-1) = temp;
-			}
-			//in anyother case, get the next iterators value 
-			//and store it in the current one
-			else
-			{
-				*it = *(it + 1);
-			}
+			temp = *it;
+		}
+		//if the iterators arrives to the last element 
+		//of the vector, puts the temp variable in it
+		if (it == chamber.end() - 1)
+		{
+			*(chamber.end() - 1) = temp;
+		}
+		//in anyother case, get the next iterators value 
+		//and store it in the current one
+		else
+		{
+			*it = *(it + 1);
 		}
 	}
 }
-void Trigger() 
+void SpinCylinder() 
 {
+	std::cout << "spinned ";
+	std::vector<int>::iterator it = chamber.begin();
+	
+	//number of time the data scrollings happens
+	for (size_t i = 0; i < rand(); i++)
+	{
+		ScrollChamber();
+	}
+}
+bool Trigger() 
+{
+	std::cout << "Trigger was pulled! \n";
 	if (chamber[0] == 1) 
 	{
-		std::cout << " got shot !";
+		ScrollChamber();
+		return true;
 	}
 	else
 	{
-		std::cout << " was lucky !";
+		ScrollChamber();
+		return false;
 	}
+	
 }
 
 int main()
 {
 	int actionIndex;
-	char exitChar = 'y';
+	char exitChar = 'n';
+	bool isDead = false;
 
 	std::cout << "Welcome to the roulette game ! \n";
 	std::cout << "Let's play !\n";
@@ -75,25 +86,54 @@ int main()
 	{
 		std::cout << "It is your turn to play !\n";
 		std::cout << "What do you want to do ?\n";
+		std::cout << "[0] To spin the chamber | [1] To Trigger \n";
 		do
 		{
-			std::cin.ignore();
+			
 			std::cin >> actionIndex;
 			switch (actionIndex)
 			{
 			case 0:
+				std::cout << "Your just ";
 				SpinCylinder();
+				std::cout << "the cylinder \n";
 				break;
 			case 1:
-				Trigger();
+				isDead = Trigger();
 				break;
 			default:
+				std::cin.ignore();
 				break;
 			}
+			
+
 		} while (actionIndex != 1);
+
+		if (isDead)
+		{
+			std::cout << "Player shot himself! \n";
+			exitChar = 'y';
+			return 0;
+		}
+		else
+		{
+			std::cout << "Player was lucky! \n";
+		}
 		std::cout << "It is the computers turn to play\n";
-		Trigger();
-		//Create a player / enemy turn situation
+		std::cout << "The computer just ";
+		SpinCylinder();
+		std::cout << "the cylinder \n";
+		isDead = Trigger();
+		if (isDead) 
+		{
+			std::cout << "Computer shot himself! \n ";
+			exitChar = 'y';
+			return 0;
+		}
+		else
+		{
+			std::cout << "Computer was lucky! \n";
+		}
 	} while (exitChar != 'y');
 }
 
