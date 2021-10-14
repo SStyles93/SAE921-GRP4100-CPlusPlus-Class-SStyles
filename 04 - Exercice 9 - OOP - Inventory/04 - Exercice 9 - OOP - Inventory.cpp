b452c588bd697a80c000c1211667297cf900aef6
SStyles93 <samuel.styles93@gmail.com>
2021-10-14 23:05:01 +0200

    05Ex

--- 04 - Exercice 9 - OOP - Inventory/04 - Exercice 9 - OOP - Inventory.cpp
+++ 04 - Exercice 9 - OOP - Inventory/04 - Exercice 9 - OOP - Inventory.cpp
@@ -9,18 +9,19 @@
 
 int main()
 {
+	//Instantiate elements
 	Inventory inventory;
 	Map map;
 	Sword sword;
 	Bow bow;
 	HealthPotion healthPotion;
 	ForcePotion forcePotion;
-
+	//Add elements to inventory
 	inventory.AddItem(&map);
 	inventory.AddItem(&sword);
 	inventory.AddItem(&bow);
 	inventory.AddItem(&healthPotion);
 	inventory.AddItem(&forcePotion);
-
+	//Display all elements in inventory
 	inventory.Display();
 }
